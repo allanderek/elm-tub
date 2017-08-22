e0be0aeee4c52de1ea7b43cea1ed7fe1522a7e82
allanderek <allan.clark@gmail.com>
2017-08-22 14:02:06 +0000

    Added a deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+# TODO: Also check that we're *on* master.
+rm js/*
+git checkout gh-pages &&
+git merge master &&
+source make.sh &&
+# TODO: check if any js files have actually changed.
+git add js/*. &&
+git commit -m "Latest build and commit to gh-pages" &&
+git push origin gh-pages &&
+git checkout master &&
+echo "Visit: https://allanderek.github.io/elm-tub/"
